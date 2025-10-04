fb9dc0e53392bb09ad43f389f4a5e88dbbdb5667
Bhakti Shinde <153360593+BhaktiS1234567@users.noreply.github.com>
2025-10-04 16:01:28 +0530

    Delete D2_Erp_Stibium/MULTITHREADING directory

--- D2_Erp_Stibium/MULTITHREADING/MainThread.java
+++
@@ -1,9 +0,0 @@
-package MULTITHREADING;
-
-public class MainThread {
-    public static void main(String[] args) {
-        Thread t = Thread.currentThread();
-        System.out.println("Current thread: " + t.getName());
-    }
-}
-
